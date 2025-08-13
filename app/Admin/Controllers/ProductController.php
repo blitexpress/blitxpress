5d1cf02eaf00bd425e1460ef94eb5dbe89185e8e
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-08-14 01:49:32 +0300

    Add editable options for home section columns in product grid

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -79,6 +79,18 @@ class ProductController extends AdminController
                 return $c ? $c->category : 'Deleted';
             })
             ->sortable();
+        //home_section_1 editable col
+        $grid->column('home_section_1', __('Home Section 1'))
+            ->editable('select', ['Yes' => 'Yes', 'No' => 'No'])
+            ->sortable();
+
+        $grid->column('home_section_2', __('Home Section 2'))
+            ->editable('select', ['Yes' => 'Yes', 'No' => 'No'])
+            ->sortable();
+
+        $grid->column('home_section_3', __('Home Section 3'))
+            ->editable('select', ['Yes' => 'Yes', 'No' => 'No'])
+            ->sortable();
 
         $grid->column('created_at', __('Created'))
             ->display(function ($created_at) {
